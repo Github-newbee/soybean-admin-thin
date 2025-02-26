adefcd12738b6ea9e8e6fd6a5f69dd189af31f82
hlb <1361888043@qq.com>
2025-02-26 15:41:52 +0800

    feat: 精简

---
+++ src/layouts/modules/global-footer/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'GlobalFooter'
+});
+</script>
+
+<template>
+  <DarkModeContainer class="h-full flex-center">Copyright MIT © 2021 后台管理系统</DarkModeContainer>
+</template>
+
+<style scoped></style>
